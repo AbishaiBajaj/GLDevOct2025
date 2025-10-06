7c516187c872fafa84f756584c837d75172f2de6
AbishaiBajaj <abishai.bajaj@automatorr.com>
2025-10-06 18:07:19 +1100

    Realty section added, lookups section filled in

---
+++ UsrRealty/Schemas/UsrRealtyTypeFormPage/UsrRealtyTypeFormPage.js
@@ -0,0 +1,124 @@
+define("UsrRealtyTypeFormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "Input_2mf6uug",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": false,
+					"label": "$Resources.Strings.UsrRealtyTypeDS_Name_6jhm778",
+					"labelPosition": "above",
+					"control": "$UsrRealtyTypeDS_Name_6jhm778"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "ColorPicker_nrmkdf2",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.ColorPicker",
+					"label": "$Resources.Strings.UsrRealtyTypeDS_UsrMyColor_3xvhbgb",
+					"labelPosition": "above",
+					"control": "$UsrRealtyTypeDS_UsrMyColor_3xvhbgb",
+					"pickerMode": "extended"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			},
+			{
+				"operation": "insert",
+				"name": "Input_ncfvbxx",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 3,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"multiline": false,
+					"label": "$Resources.Strings.UsrRealtyTypeDS_Description_gzrub7v",
+					"labelPosition": "above",
+					"control": "$UsrRealtyTypeDS_Description_gzrub7v"
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 2
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"UsrRealtyTypeDS_Name_6jhm778": {
+						"modelConfig": {
+							"path": "UsrRealtyTypeDS.Name"
+						}
+					},
+					"UsrRealtyTypeDS_UsrMyColor_3xvhbgb": {
+						"modelConfig": {
+							"path": "UsrRealtyTypeDS.UsrMyColor"
+						}
+					},
+					"UsrRealtyTypeDS_Description_gzrub7v": {
+						"modelConfig": {
+							"path": "UsrRealtyTypeDS.Description"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {
+						"UsrRealtyTypeDS": {
+							"type": "crt.EntityDataSource",
+							"scope": "page",
+							"config": {
+								"entitySchemaName": "UsrRealtyType",
+								"loadParameters": {
+									"options": {
+										"pagingConfig": {
+											"rowCount": 1,
+											"rowsOffset": -1
+										},
+										"sortingConfig": {
+											"columns": []
+										}
+									}
+								},
+								"allowCopyingRecords": false
+							}
+						}
+					},
+					"primaryDataSourceName": "UsrRealtyTypeDS"
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
